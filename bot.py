001aede86eedbd94838bbdd5f1883a0b0f96e95e
Oliver <oliver.saravanja@gmail.com>
2020-11-08 21:50:57 +0100

    v0.2 - Added §reddit and §okbr (even totally safe now with NSFW check). Can also now define user_ids of the owner in owner_ids and lock specific commands for development.

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ from discord.ext import commands
 from dotenv import load_dotenv
 from datetime import datetime
 from varname import Wrapper
+import praw
+import random
 
 
 # COLORS FOR CONSOLE ERRORS
@@ -22,24 +24,27 @@ class bColors:
 # LOAD ENV VARIABLES FROM .ENV (NOT MANAGED THROUGH VCS)
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+R_SECRET = os.getenv('REDDIT_SECRET')
+R_ID = os.getenv('REDDIT_ID')
+R_USERNAME = os.getenv('REDDIT_USERNAME')
+R_PASSWORD = os.getenv('REDDIT_PASSWORD')
 
 # NECESSARY BOT STUFF
 bot = commands.Bot(command_prefix='§', description="ANTICOMPSCI")
 bot.remove_command('help')
+r_client = praw.Reddit(
+    client_secret=R_SECRET,
+    client_id=R_ID,
+    user_agent='discordbot:R_ID:v0.2 (by u/Remaked)',
+    username=R_USERNAME,
+    password=R_PASSWORD
+)
+owner_ids = ['108305736131440640']
 
 
 # BACKGROUND LOOP
 async def background_loop():
-    await bot.wait_until_ready()
-    while not bot.is_closed():
-        await bot.change_presence(
-            activity=discord.CustomActivity(
-                name='Anti Computer Science Action',
-                type=discord.ActivityType.custom,
-                state='Current Time:',
-                details=datetime.now().strftime('#H h #M m #S s')
-                )
-            )
+    return
 
 
 # EVENT HANDLING
@@ -56,18 +61,185 @@ async def on_ready():
 
 @bot.event
 async def on_command_error(_, error):
-    print(bColors.FAIL + "COMMAND ERROR: " + bColors.ENDC + bColors.UNDERLINE + str(error))
+    print(bColors.FAIL + "COMMAND ERROR: " + bColors.ENDC + str(error))
 
 
 # COMMANDS
 utility = Wrapper(['ping', 'echo'])
-coms = [utility]
+media = Wrapper(['reddit', 'okbr'])
+coms = [media, utility]
+
+
+@bot.command(name='okbr')
+async def r_okbr(ctx):
+    """
+    if owner_ids.__contains__(ctx.message.author.id):
+        await ctx.send('Currently locked for development. :face_vomiting:')
+        return
+    """
+
+    try:
+        okbr = r_client.subreddit('okbuddyretard')
+        posts = list(okbr.hot())
+        post = random.choice(posts)
+        n = 0
+        while post.over_18:
+            if n < 50:
+                post = random.choice(posts)
+            else:
+                break
+            n += 1
+        if post.over_18:
+            print(bColors.WARNING + 'Somebody is trying to post NSFW stuff on Discord!')
+            await ctx.send('Couldn\'t find any non-NSFW post. Try again.')
+            return
+
+        to_embed = discord.Embed(
+            title=post.title,
+            url=post.url,
+            color=discord.Colour.from_rgb(55, 133, 245)
+        )
+        to_embed.set_author(
+            name=post.author.name,
+            icon_url=post.author.icon_img
+        )
+
+        to_embed.set_footer(
+            text='from: r/' + okbr.display_name + ' | UP: ' + str(post.score) + ' | DOWN: '
+                 + str(round(post.score * (1 - post.upvote_ratio))) + ' | Comments: ' + str(post.num_comments)
+        )
+
+        if post.is_self:
+            to_embed.description = post.selftext
+        else:
+            to_embed.set_image(
+                url=post.url
+            )
+        await ctx.send(embed=to_embed)
+    except Exception as e:
+        print(bColors.FAIL + 'REDDIT EXCEPTION: ' + bColors.ENDC + str(e))
+        await ctx.send('Something went wrong with your request. Sorry...')
+
+
+@bot.command(name='reddit')
+async def reddit(ctx, arg):
+    """
+    if owner_ids.__contains__(ctx.message.author.id):
+        await ctx.send('Currently locked for development. :face_vomiting:')
+        return
+    """
+
+    try:
+        if str(arg) == 'random':
+            sub = r_client.random_subreddit()
+        else:
+            sub = r_client.subreddit(arg)
+
+        if sub.over18:
+            print(bColors.WARNING + 'Somebody is trying to post NSFW stuff on Discord!')
+            await ctx.send('Not going to browse _r/' + arg + '_ on my christian server. :flushed:')
+            return
+
+        posts = list(sub.top(time_filter='month'))
+        post = random.choice(posts)
+        n = 0
+        while post.over_18:
+            if n < 50:
+                post = random.choice(posts)
+            else:
+                break
+            n += 1
+        if post.over_18:
+            print(bColors.WARNING + 'Somebody is trying to post NSFW stuff on Discord!')
+            await ctx.send('Couldn\'t find any non-NSFW post. Maybe another subreddit?')
+            return
+
+        to_embed = discord.Embed(
+            title=post.title,
+            url=post.url,
+            color=discord.Colour.from_rgb(55, 133, 245)
+        )
+        to_embed.set_author(
+            name=post.author.name,
+            icon_url=post.author.icon_img
+        )
+
+        to_embed.set_footer(
+            text='from: r/' + sub.display_name + ' | UP: ' + str(post.score) + ' | DOWN: '
+                 + str(round(post.score*(1-post.upvote_ratio))) + ' | Comments: ' + str(post.num_comments)
+        )
+        if post.is_self:
+            if len(post.selftext) < 2000:
+                text = post.selftext[0:1985] + '... **read more on reddit**'
+            else:
+                text = post.selftext
+            to_embed.description = text
+        else:
+            if str(post.url).__contains__('.gifv'):
+                # TODO: GET GIFV TO WORK
+                await ctx.send('Sorry, no support for .gifv files yet')
+                return
+            else:
+                to_embed.set_image(
+                    url=post.url
+                )
+        await ctx.send(embed=to_embed)
+    except Exception as e:
+        print(bColors.FAIL + 'REDDIT EXCEPTION: ' + bColors.ENDC + str(e))
+        if str(e).__contains__('404') or str(e).__contains__('search'):
+            await ctx.send('It seems the subreddit you supplied doesn\'t exist...')
+        else:
+            await ctx.send('Something went wrong with your request. Sorry...')
+
+
+@bot.command(name='ping')
+async def ping(ctx):
+    try:
+        latency = str(round(bot.latency*1000)) + " ms"
+        to_embed = discord.Embed(
+            title=':ping_pong: Pong!',
+            description='Latency in ms: `' + str(latency) + '`',
+            color=discord.Colour.from_rgb(255, 0, 0),
+        )
+        to_embed.set_footer(text='As of: ' + datetime.now().strftime('%a, %d.%m.%Y, %H:%M'))
+        await ctx.send(embed=to_embed)
+    except Exception as e:
+        print(e)
+        await ctx.send("Couldn't print latency because ollie sucks at coding.")
+
+
+@bot.command(name='pong')
+async def ping(ctx):
+    try:
+        latency = str(round(bot.latency*1000)) + " ms"
+        to_embed = discord.Embed(
+            title=':ping_pong: Ping!',
+            description='Latency in ms: `' + str(latency) + '`',
+            color=discord.Colour.from_rgb(255, 0, 0),
+        )
+        to_embed.set_footer(text='As of: ' + datetime.now().strftime('%a, %d.%m.%Y, %H:%M'))
+        await ctx.send(embed=to_embed)
+    except Exception as e:
+        print(e)
+        await ctx.send("Couldn't print latency because ollie sucks at coding.")
+
+
+@bot.command(name='echo')
+async def test(ctx, *args):
+    out = ''
+    for a in args:
+        out += a + ' '
+    await ctx.send(out)
 
 
 @bot.command(name='help')
-async def myHelp(ctx, *args):
+async def myhelp(ctx, *args):
     to_embed = discord.Embed(
-        color=discord.Colour.from_rgb(255, 0, 0)
+        color=discord.Colour.from_rgb(255, 0, 0),
+    )
+    trans_logo = discord.File(os.path.abspath('Logo_transparent.png'), filename='image.png')
+    to_embed.set_thumbnail(
+        url='attachment://image.png'
     )
     if len(args) == 0:
         to_embed.title = 'ANTICOMPSCI Commands'
@@ -90,51 +262,26 @@ async def myHelp(ctx, *args):
         com = args.__getitem__(0)
         to_embed.title = '§' + com
         if com == 'ping':
-            to_embed.description = 'Used to return latency of bot.'
+            to_embed.description = 'Correct form: `§ping` \n' \
+                                   'Used to return latency of the bot.'
             to_embed.set_footer(text='Alternative version: §pong')
         elif com == 'pong':
-            to_embed.description = 'Used to return latency of bot.'
+            to_embed.description = 'Correct form: `§pong` \n' \
+                                   'Used to return latency of the bot.'
             to_embed.set_footer(text='Alternative version: §ping')
         elif com == 'echo':
-            to_embed.description = 'Largely for debug purposes. Simply returns your message 1:1.'
+            to_embed.description = 'Correct form: `§echo` \n' \
+                                   'Largely for debug purposes. Simply returns your message 1:1.'
+        elif com == 'reddit':
+            to_embed.description = 'Correct form: `§reddit <sub>`\n' \
+                                   'Returns a random post from subreddit specified in `<sub>`\n' \
+                                   '(Text or Image)'
+            to_embed.set_footer(text='Use \"random\" as your <sub> for a random subreddit')
+        elif com == 'okbr':
+            to_embed.description = 'Correct form: `§okbr`\n' \
+                                   'Returns a random post from r/okbuddyretard\n'
 
-    await ctx.send(embed=to_embed)
+    await ctx.send(file=trans_logo, embed=to_embed)
 
-
-@bot.command(name='ping')
-async def ping(ctx):
-    try:
-        latency = str(round(bot.latency*1000)) + " ms"
-        to_embed = discord.Embed(
-            title=':ping_pong: Pong!',
-            description='Latency in ms: ' + str(latency),
-            color=discord.Colour.from_rgb(255, 0, 0),
-        )
-        to_embed.set_footer(text='As of: ' + datetime.now().strftime('%a, %d.%m.%Y, %H:%M'))
-        await ctx.send(embed=to_embed)
-    except Exception as e:
-        print(e)
-        await ctx.send("Couldn't print latency because ollie sucks at coding.")
-
-
-@bot.command(name='pong')
-async def ping(ctx):
-    try:
-        latency = str(round(bot.latency*1000)) + " ms"
-        to_embed = discord.Embed(
-            title=':ping_pong: Ping!',
-            description='Latency in ms: ' + str(latency),
-            color=discord.Colour.from_rgb(255, 0, 0),
-        )
-        to_embed.set_footer(text='As of: ' + datetime.now().strftime('%a, %d.%m.%Y, %H:%M'))
-        await ctx.send(embed=to_embed)
-    except Exception as e:
-        print(e)
-        await ctx.send("Couldn't print latency because ollie sucks at coding.")
-
-
-@bot.command(name='echo')
-async def test(ctx, arg):
-    await ctx.send(arg)
 
 bot.run(TOKEN)
